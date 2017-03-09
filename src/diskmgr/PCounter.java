866267562ffb9c25c456e3e16bec410c36bae202
hadoop <hadoop@hadoop-HP-Notebook>
2017-03-08 17:11:16 -0700

    Task 2.9 part1

---
+++ src/diskmgr/PCounter.java
@@ -0,0 +1,22 @@
+package diskmgr;
+
+public class PCounter
+{
+	public static int readCounter;
+	public static int writeCounter;
+	
+	public static void initialize()
+	{
+		readCounter=0;
+		writeCounter=0;
+	}
+	public static void readIncrement()
+	{
+		readCounter++;
+	}
+	public static void writeIncrement()
+	{
+		writeCounter++;
+	}
+		
+}
