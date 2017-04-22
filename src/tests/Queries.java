e451500b93afb4b252620bb5f68779c9ed09f528
Mihir Thakkar <mihir9293@gmail.com>
2017-04-22 03:13:58 -0700

    Add Pushkar's function

--- src/tests/Queries.java
+++ src/tests/Queries.java
@@ -287,6 +287,148 @@ public class Queries {
         
         return ansNidsSorted;
     }
+    
+    public static List<EdgeQueryPojo> queryPE2(String name,List<EdgeQueryPojo> edgeQuery) throws JoinsException, IndexException, PageNotReadException, TupleUtilsException, PredEvalException, SortException, LowMemException, UnknowAttrType, UnknownKeyTypeException, Exception
+    {
+        List<EdgeQueryPojo> ansNids= new LinkedList<EdgeQueryPojo>();
+        List<EdgeQueryPojo> childQuery = new LinkedList<EdgeQueryPojo>();
+        List<NodeQueryPojo> nchildQuery = new LinkedList<NodeQueryPojo>();
+        int i=0;
+        
+        while(edgeQuery.size()>1)
+        {
+              List<EdgeQueryPojo> children = new LinkedList<EdgeQueryPojo>();
+            if(i==0)
+            {
+                
+                NID nid=edgeQuery.get(0).getNd();
+                String node_label=SystemDefs.JavabaseDB.nhfile.getNode(nid).getLabel();
+                NodeQueryPojo npj=new NodeQueryPojo();
+                npj.setLabel(node_label);
+                nchildQuery.add(npj);
+                Iterator<NodeQueryPojo> iterQuery = nchildQuery.iterator();
+                while (iterQuery.hasNext()) {
+                    NestedLoopsJoins nlj;
+
+                    nlj  = new Join().joinNodeSEdge(name, iterQuery.next());
+                    Tuple t = new Tuple();
+                    iterator.Iterator joinResult=(iterator.Iterator)nlj;
+                    while ((t = joinResult.get_next()) != null) {
+                        EdgeQueryPojo tmp=new EdgeQueryPojo();
+                        tmp.setEdgelabel(t.getStrFld(6)); 
+                        NID destNid = new NID(); 
+                        destNid.pageNo=new PageId(t.getIntFld(4));
+                        destNid.slotNo=t.getIntFld(5);
+                        String label=t.getStrFld(6);
+                        int wt=t.getIntFld(1);
+                        if(edgeQuery.get(1).getKey()==1)
+                        {
+                            if(edgeQuery.get(1).getEdgelabel().equalsIgnoreCase(label))
+                            {
+                                children.add(tmp);
+                                if(edgeQuery.size()==2)
+                                {
+                                    EdgeQueryPojo destNidPojo = new EdgeQueryPojo();
+                                    destNidPojo.setNd(destNid);
+                                    ansNids.add(destNidPojo);
+                                }
+                            }
+                            
+                        }
+                       else if(edgeQuery.get(1).getKey()==2)
+                       {
+                           if(edgeQuery.get(1).getWeight()>=wt)
+                           {
+                               children.add(tmp);
+                               if(edgeQuery.size()==2)
+                               {
+                                   EdgeQueryPojo destNidPojo = new EdgeQueryPojo();
+                                   destNidPojo.setNd(destNid);
+                                   ansNids.add(destNidPojo);
+                               }
+                           }
+                       }
+                        
+                    }
+                    
+                    
+                }
+               
+            }
+            else{
+                
+                EdgeQueryPojo epj=new EdgeQueryPojo();
+                Iterator<EdgeQueryPojo> iterQuery=childQuery.iterator();
+                while (iterQuery.hasNext()) {
+                    SortMerge sm=new  Join().joinEdgeEdge(name, iterQuery.next());
+                     Tuple t = new Tuple();
+                     while ((t = sm.get_next()) != null) {
+                         EdgeQueryPojo tmp=new EdgeQueryPojo(); 
+                         String label=t.getStrFld(6);
+                         tmp.setEdgelabel(label);
+                         NID destNid = new NID(); 
+                        //System.out.println("label is "+t.getStrFld(6));
+                          destNid.pageNo=new PageId(t.getIntFld(4));
+                          destNid.slotNo=t.getIntFld(5);
+                          
+                          int wt=t.getIntFld(1);
+                          if(edgeQuery.get(1).getKey()==1)
+                          {
+                            if(edgeQuery.get(1).getEdgelabel().equalsIgnoreCase(label))
+                            {
+                                children.add(tmp);
+                                if(edgeQuery.size()==2)
+                                  {
+                                      EdgeQueryPojo destNidPojo = new EdgeQueryPojo();
+                                      destNidPojo.setNd(destNid);
+                                      ansNids.add(destNidPojo);
+                                  }
+                            }
+                            
+                            
+                          }
+                          else if(edgeQuery.get(1).getKey()==2)
+                          {
+                           if(edgeQuery.get(1).getWeight()>=wt)
+                           {
+                               children.add(tmp);
+                               if(edgeQuery.size()==2)
+                                  {
+                                      EdgeQueryPojo destNidPojo = new EdgeQueryPojo();
+                                      destNidPojo.setNd(destNid);
+                                      ansNids.add(destNidPojo);
+                                  }
+                           }
+                          }
+                         
+                     }
+                     try {
+                        sm.close();
+                      Join.f.deleteFile();
+                       Join.fN.deleteFile();
+                    } catch (Exception e) {
+                        
+                        e.printStackTrace();
+                    }
+                }
+                
+                
+            }
+            //System.out.println("children in iteration"+i);
+            
+        
+            childQuery=children;
+            /*for(EdgeQueryPojo epj:childQuery)
+            {
+                System.out.println(epj.getEdgelabel());
+            }*/
+            edgeQuery.remove(0);
+            i++;
+        }
+        
+       
+        return ansNids;
+    }
 }
 
 
